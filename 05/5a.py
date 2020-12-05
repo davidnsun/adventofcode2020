d46642f9036b56fed01c3adfff4a6d0925cc7ee5
7240 <dnsun@andrew.cmu.edu>
2020-12-05 00:33:33 -0500

    Solve day 5

---
+++ 05/5a.py
@@ -0,0 +1,48 @@
+import re
+import sys
+from collections import Counter, defaultdict, deque
+from itertools import permutations, combinations, product
+import itertools
+
+def compute_row(s):
+    lo = 0
+    hi = 127
+    n = len(s)
+    result = 0
+    for i in range(n):
+        c = s[i]
+        mid = (hi - lo)//2
+        if c == "F":
+            hi = lo + mid
+        elif c == "B":
+            lo = hi - mid
+        if i == n-1:
+            if c == "F":
+                result = lo
+            else:
+                result = hi
+    return result
+
+def compute_col(s):
+    lo = 0
+    hi = 7
+    n = len(s)
+    result = 0
+    for i in range(n):
+        c = s[i]
+        mid = (hi - lo)//2
+        if c == "L":
+            hi = lo + mid
+        elif c == "R":
+            lo = hi - mid
+        if i == n-1:
+            if c == "L":
+                result = lo
+            else:
+                result = hi
+    return result
+
+with open("input.txt") as input_file:
+    lines = input_file.read().split("\n")
+L = sorted([compute_row(line[:-3])*8+compute_col(line[-3:]) for line in lines])
+print(max(L))
