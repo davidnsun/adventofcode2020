e69b781db716d18bf51b174f55d078322a60c5bc
7240 <dnsun@andrew.cmu.edu>
2020-12-06 00:29:39 -0500

    Solve day 6

--- 06/6b.py
+++ 06/6b.py
@@ -2,11 +2,10 @@ input_file = open("input.txt")
 lines = input_file.read().split("\n\n")
 input_file.close()
 result = 0
-a = set("abcdefghijklmnopqrstuvwxyz")
 for line in lines:
     L = line.split()
-    fst = set(L[0])
+    inter = set(L[0])
     for i in range(1,len(L)):
-        fst = fst.intersection(set(L[i]))
-    result += len(fst)
+        inter = inter.intersection(set(L[i]))
+    result += len(inter)
 print(result)
