a98be78fe768ff4a52739fde5a4e2974cc78de44
7240 <dnsun@andrew.cmu.edu>
2020-12-06 06:30:48 -0500

    Solve day 6

--- 06/6a.py
+++ 06/6a.py
@@ -3,3 +3,5 @@ lines = input_file.read().split("\n\n")
 input_file.close()
 L = [len(set("".join(line.split()))) for line in lines]
 print(sum(L))
+
+print(sum([len(set("".join(line.split()))) for line in open("input.txt").read().split("\n\n")]))
--- 06/6b.py
+++ 06/6b.py
@@ -9,3 +9,28 @@ for line in lines:
         inter = inter.intersection(set(L[i]))
     result += len(inter)
 print(result)
+
+# >>> from functools import reduce
+# >>> list(map(set,["ab","ac"]))
+# [{'b', 'a'}, {'c', 'a'}]
+# >>> reduce(lambda x,y : x & y, list(map(set,["ab","ac"])))
+# {'a'}
+
+from functools import reduce
+input_file = open("input.txt")
+lines = input_file.read().split("\n\n")
+input_file.close()
+result = 0
+for line in lines: # line = "ab\nac"
+    L = line.split() # L = ["ab","ac"]
+    l = list(map(set,L)) # l = [{'a', 'b'}, {'a', 'c'}]
+    result += len(reduce(lambda x,y : x & y, l)) # reduce yields {'a'}
+print(result)
+
+from functools import reduce
+with open("input.txt") as input_file:
+    lines = input_file.read().split("\n\n")
+print(sum([len(reduce(lambda x,y : x & y, list(map(set,line.split())))) for line in lines]))
+
+from functools import reduce
+print(sum([len(reduce(lambda x,y : x & y, list(map(set,line.split())))) for line in open("input.txt").read().split("\n\n")]))
