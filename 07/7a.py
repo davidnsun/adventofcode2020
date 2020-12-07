2936e60fd1de06989cb479c586c7003abcedb8dc
7240 <dnsun@andrew.cmu.edu>
2020-12-07 01:55:54 -0500

    Solve day 7

--- 07/7a.py
+++ 07/7a.py
@@ -3,14 +3,18 @@ lines = input_file.read().split("\n")
 input_file.close()
 lines = lines[:-1] # get rid of last ['']
 target = "shiny gold"
-result = set()
 # starting point incl all that mention shiny gold but isn't shiny gold itself
-for line in lines:
-    L = line.split(" bags contain ")
-    if L[0] == target:
-        continue
-    if target in L[1]:
-        result.add(L[0])
+
+result = set()
+def containers(target,result):
+    for line in lines:
+        L = line.split(" bags contain ")
+        if L[0] == target:
+            continue
+        if target in L[1]:
+            result.add(L[0])
+    return result
+result = containers(target,result)
 
 # look for all of those in the latter half of each line too and add the bags
 # containing them
@@ -19,12 +23,7 @@ while True: # BRRRRRRRRR
     before = len(result)
     new = set()
     for target in result:
-        for line in lines:
-            L = line.split(" bags contain ")
-            if L[0] == target:
-                continue
-            if target in L[1]:
-                new.add(L[0])
+        new = containers(target,new)
     result = result.union(new)
     after = len(result)
     if before == after:
